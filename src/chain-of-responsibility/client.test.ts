5dd59981b4777aac2b6de8e3c9b15c485cc59f5f
NagyEndre <endre.tamas.nagy@gmail.com>
2022-10-31 22:22:47 +0100

    TEST(client): Add test and refactor existing

--- src/chain-of-responsibility/client.test.ts
+++ src/chain-of-responsibility/client.test.ts
@@ -1,26 +1,46 @@
 import { ArgumentValidator } from "./ArgumentValidator";
 import { Authenticator } from "./Authenticator";
+import { Handler } from "./BaseHandler";
+import RobotController, { RobotOperation } from "./RobotController";
 
 describe("Client POV test of Chain-Of-Responsibility pattern.", () => {
-  test.each([null, undefined])("Should throw on invalid arguments", (arg) => {
-    const h1 = new ArgumentValidator();
-    const h2 = new Authenticator();
-    h1.setNext(h2);
+  let firstHandler: Handler;
 
+  beforeEach(() => {
+    firstHandler = new ArgumentValidator();
+    const h2 = new Authenticator();
+    const h3 = new RobotController();
+    firstHandler.setNext(h2);
+    h2.setNext(h3);
+  });
+
+  test.each([null, undefined])("Should throw on invalid arguments", (arg) => {
     expect(() => {
-      h1.handle(arg);
+      firstHandler.handle(arg);
     }).toThrow("Invalid argument.");
   });
 
   test("Should grant permission when correct credential specified.", () => {
-    const h1 = new ArgumentValidator();
-    const h2 = new Authenticator();
-    h1.setNext(h2);
-
     console.log = jest.fn();
 
-    h1.handle({ user: "Batman", password: "BatCave" });
+    firstHandler.handle({
+      user: "Batman",
+      password: "BatCave",
+      operation: RobotOperation.Reset,
+    });
 
     expect(console.log).toHaveBeenCalledWith("Access granted. Welcome.");
+  });
+
+  test("Should start robot program.", () => {
+    console.log = jest.fn();
+
+    firstHandler.handle({
+      user: "Batman",
+      password: "BatCave",
+      operation: RobotOperation.Start,
+    });
+
+    expect(console.log).toHaveBeenCalledWith("Starting robot program.");
   });
 });
